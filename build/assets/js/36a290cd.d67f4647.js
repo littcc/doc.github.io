f35acd1a84f8836e4949690d172e6f42ab02d7d4
littcc <littcc@icloud.com>
2021-04-01 11:40:22 +0800

    refactor: build

---
+++ build/assets/js/36a290cd.d67f4647.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{80:function(s){s.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"test","name":"test","count":1,"permalink":"/blog/tags/test"}')}}]);
---
+++ build/assets/js/a7023ddc.0163b47f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{93:function(t){t.exports=JSON.parse('{"test":{"allTagsPath":"/blog/tags","slug":"test","name":"test","count":1,"permalink":"/blog/tags/test"},"y-taro-ui":{"allTagsPath":"/blog/tags","slug":"y-taro-ui","name":"y-taro-ui","count":1,"permalink":"/blog/tags/y-taro-ui"}}')}}]);
